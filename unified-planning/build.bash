6a3196890905e5cce4548754185c9083403d07c5
iocchi <iocchi@diag.uniroma1.it>
2022-12-22 15:06:29 +0100

    build date time

--- unified-planning/build.bash
+++ unified-planning/build.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 UPAR="--build-arg UID=`id -u` --build-arg GID=`id -g`"
+TPAR="--build-arg BUILDDATETIME=`date +"%Y%m%d_%H%M%S"`"
 
-docker build $UPAR -t unifiedplanning -f Dockerfile .
+docker build $UPAR $TPAR -t unifiedplanning -f Dockerfile .
 
